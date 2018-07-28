99d4638df6bbdc7a9a5b2f3d7e3bdb8eb7177f9c
EC2 Default User <testtest@gmail.com>
2018-07-28 07:08:01 +0000

    CRUD tasklist

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Tasks;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+public function index()
+    {
+        $tasklist = Tasks::all();
+        
+        return view('tasks.index',[
+            'tasklist' => $tasklist
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $tasklist = new tasks;
+        
+        return view('tasks.create',[
+          'tasklist' => $tasklist,
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $tasklist = new tasks;
+        $tasklist->content = $request->content;
+        $tasklist->save();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $tasklist = Tasks::find($id);
+        
+        return view('tasks.show', [
+            'tasklist' => $tasklist
+            ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tasklist = Tasks::find($id);
+        
+        return view('tasks.edit',[
+            'tasklist' => $tasklist
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $tasklist = Tasks::find($id);
+        $tasklist->content = $request->content;
+        $tasklist->update();
+        
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $tasklist = Tasks::find($id);
+        $tasklist->delete();
+        
+        return redirect('/');
+    }
+}
