f37310f65584bc2b08bf77df1debd03d785988bb
EC2 Default User <testtest@gmail.com>
2018-08-14 00:32:13 +0000

    Top Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
         <title>Tasklist</title>
 
         <!-- Bootstrap -->
@@ -12,13 +13,13 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         
     </head>
-
     <body>
         @include('commons.navbar')
+        
             <div class="container">
-            @include('commons.error_tasks')
+                @include('commons.error_tasks')
             
-            @yield('content')
+                @yield('content')
             </div>
     </body>
 </html>
